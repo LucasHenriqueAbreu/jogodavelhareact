85c58fd159f6692ee7e05eb53c06d551b7d99fdd
lucas.henrique <lucas.henrique@viasoft.com.br>
2017-12-21 18:05:53 -0200

    Adicionado compoenents

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Game from './game/Game';
+
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Jogo da velha em react</h1>
-        </header>
-        <p className="App-intro">
-          Vá para o diabo <code>src/App.js</code> satanás.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<img src={logo} className="App-logo" alt="logo" />
+					<h1 className="App-title">Jogo da velha em react</h1>
+				</header>
+				<div className="Content">
+					<Game></Game>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
