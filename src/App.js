de03b3a653e54a431ee22f64513e25972cdd8124
lucas.henrique <lucas.henrique@viasoft.com.br>
2017-12-22 17:04:21 -0200

    Alterado stilos

--- src/App.js
+++ src/App.js
@@ -7,17 +7,22 @@ import Game from './game/Game';
 class App extends Component {
 	render() {
 		return (
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Jogo da velha em react</h1>
-				</header>
-				<div className="Content">
-					<Game></Game>
+			<div>
+				<div className="App">
+					<header className="App-header">
+						<img src={logo} className="App-logo" alt="logo" />
+						<h1 className="App-title">Jogo da velha em React</h1>
+					</header>
+					<div className="Content">
+						<Game></Game>
+					</div>
 				</div>
+				<footer>
+					<strong><a href="https://github.com/LucasHenriqueAbreu">Lucas Henrique de Abreu</a></strong>
+					lucasigual14@gmail.com
+				</footer>
 			</div>
 		);
 	}
 }
-
 export default App;
