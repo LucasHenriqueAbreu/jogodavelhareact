8727e705518da886f413aae87fdc73b8149d9ec2
lucas.henrique <lucas.henrique@viasoft.com.br>
2017-12-29 17:58:24 -0200

    adicionado login com facebook

--- client/src/login/Login.js
+++ client/src/login/Login.js
@@ -3,45 +3,88 @@ import axios from 'axios';
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 
-class Login extends Component {
+class Login extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        };
+        this.handleFBLogin = this.handleFBLogin.bind(this);
+    }
+
+    loadFbLoginApi() {
+
+        window.fbAsyncInit = function () {
+            /*global FB*/
+            FB.init({
+                appId: 1957892444499542, //FB_APP_ID,
+                cookie: true,  // enable cookies to allow the server to access
+                // the session
+                xfbml: true,  // parse social plugins on this page
+                version: 'v2.5' // use version 2.1
+            });
+        };
+
+        console.log("Loading fb api");
+        // Load the SDK asynchronously
+        (function (d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    }
+
+    componentDidMount() {
+        this.loadFbLoginApi();
+    }
+
+    fbLogin() {
+        FB.login(result => {
+            if (result.authResponse) {
+                return this.http.post(`http://localhost:3000/api/v1/auth/facebook`, { access_token: result.authResponse.accessToken })
+                    .toPromise()
+                    .then(response => {
+                        var token = response.headers.get('x-auth-token');
+                        if (token) {
+                            localStorage.setItem('id_token', token);
+                        }
+                        resolve(response.json());
+                    })
+                    .catch(() => reject());
+            } else {
+                reject();
+            }
+        }, { scope: 'public_profile,email' })
+    }
+
+    logout() {
+        localStorage.removeItem('id_token');
+    }
+
+    isLoggedIn() {
+        return new Promise((resolve, reject) => {
+            this.getCurrentUser().then(user => resolve(true)).catch(() => reject(false));
+        });
+    }
+
+    getCurrentUser() {
+        return new Promise((resolve, reject) => {
+            return this.http.get(`http://localhost:3000/api/v1/auth/me`).toPromise().then(response => {
+                resolve(response.json());
+            }).catch(() => reject());
+        });
+    }
+
     render() {
         return (
             <div>
-                <div className="App">
-                    <header className="App-header">
-                        <h1 className="App-title">Entrar</h1>
-                    </header>
-                    <div className="Content">
-                        <form role="form">
-                            <TextField
-                                type="text"
-                                hintText="usuário"
-                                errorText="Usuário é necessário"
-                                floatingLabelText="informe seu usuário"
-                            /><br />
-                            <TextField
-                                type="password"
-                                hintText="Senha"
-                                errorText="Senha é necessário"
-                                floatingLabelText="Informe sua senha"
-                            /><br />
-                            <RaisedButton onClick={this.login} label="Login" primary={true} />
-                        </form>
-
-                        <p>Precisa de conta? <a href="/register">Cadastro</a></p>
-                        <p>Or go <a href="/">Home</a>.</p>
-                    </div>
-                </div>
+                <RaisedButton classNames="btn-facebook"
+                    id="btn-social-login"
+                    onClick={this.fbLogin} label="Sign in with Facebook" primary={true} />
             </div>
-        )
-    }
-
-    login() {
-        axios.post(`http://192.168.3.209:1337/auth/local`, )
-            .then(res => {
-                console.log(res);
-                // this.setState({ posts });
-            });
+        );
     }
 }
 
